460a062873518a6d0d3b4ce3efa56478197fa889
Martijn Arts <martijn@martijnarts.com>
2020-06-08 02:51:25 +0200

    Fix local rename

--- local-rename-to-remote.sh
+++ local-rename-to-remote.sh
@@ -5,13 +5,13 @@ DIR=$1
 
 cd $DIR
 REMOTE=$(git remote get-url origin)
-USER=$(echo $REMOTE | sed 's/.*github\.com[\/:]\([[:alnum:]]\+\).*/\1/')
-REPO=$(echo $REMOTE | sed 's/.*github\.com[\/:][[:alnum:]]\+\/\([[:alnum:]]\+\).*/\1/')
+USER=$(echo $REMOTE | sed 's/.*github\.com[\/:]\([[:alnum:]-]\+\).*/\1/')
+REPO=$(echo $REMOTE | sed 's/.*github\.com[\/:][[:alnum:]-]\+\/\([[:alnum:]-]\+\).*/\1/')
 
 FROM=$2
 TO=$(gh api "repos/$USER/$REPO" | jq -r '.default_branch')
 
 set -x
 git branch $FROM -m $TO
-git branch $TO -u origin/$FROM
+git branch $TO -u origin/$TO
 cd -
